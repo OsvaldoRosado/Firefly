52ab1d6ec5497632804ed8a81593f67ac1542f24
Jon <goodnow.jonathan@gmail.com>
2015-09-30 21:01:50 -0400

    Added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    ts: {
+      default : {
+        src: ["public/*.ts"]
+      }
+    },
+    watch: {
+        ts: {
+            files: ["public/*.ts"],
+            tasks: ["ts"]
+        }
+    }
+  });
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.registerTask("default", ["ts", "watch"]);
+};
