d0d3132bd96b8eb77fc4d41205e2a5a514427c73
Devin Nguyen <nguyed5@rpi.edu>
2015-11-09 01:37:50 -0500

    add drag drop zone and animated loading bar

--- public/js/uploader/upload.js
+++ public/js/uploader/upload.js
@@ -1,18 +1,41 @@
 $(function() {
+
+	var filename;
 	
 	$.get("/api/getCurrentUserInfo?rand="+(new Date().getTime()),function(data) {
 		data = JSON.parse(data);
 		if(!data.success){
 			window.location.replace("/");
-		}else{
+		} else{
 			$(".username").text(data.data.name);
 		}
 	});
 	
-	$('form#uploadForm').submit(function(e) {
+	var dropzone = $(".presentationDropZone");
+	dropzone.on('dragenter', function(e) {
+		e.stopPropagation();
 		e.preventDefault();
-		var formData = new FormData($('form#uploadForm')[0]);
+		dropzone.toggleClass("highlight");
+	});
+	dropzone.on('dragleave', function(e) {
+		dropzone.toggleClass('highlight');
+		$(".presentationDropZone h1").html("Drop Presentation Here");
+	});
+	dropzone.on('dragover', function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		$(".presentationDropZone h1").html("Let's go!");
+	});
+	dropzone.on('drop', function(e) {
+		dropzone.addClass('highlight');
+		e.preventDefault();
+		var file = e.originalEvent.dataTransfer.file;
+		filename = e.originalEvent.dataTransfer.files[0].name;
+		uploadPresentation(file, dropzone);
+	});
 
+	function uploadPresentation(file, status) {
+		var formData = new FormData($('form#uploadForm')[0]);
 		$.ajax({
 			url: '/api/uploadPresentation',
 			type: 'POST',
@@ -20,7 +43,7 @@ $(function() {
 				var presXhr = $.ajaxSettings.xhr();
 				if (presXhr.upload) {
 					presXhr.upload.addEventListener('progress', progressHandler, false);
-					$(".upstart").val("Please wait...");
+					$(".upstart").html("Uploading " + filename + "...");
 					$(".upstart").attr("disabled","disabled");
 				}
 				return presXhr;
@@ -46,12 +69,14 @@ $(function() {
 			contentType: false,
 			processData: false
 		});
-		return false;
-	});
+	}
 
 	function progressHandler(e) {
 		if (e.lengthComputable) {
 			$('progress').attr({value: e.loaded, max:e.total});
+			if (e.loaded == e.total) {
+				$(".upstart").html("Converting " + filename + "...");
+			}
 		}
 	}
 });
